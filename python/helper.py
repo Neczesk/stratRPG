b47e8f76d5658c158faa534129461af1976c065d
kyle <haltermann.kyle@gmail.com>
2021-05-07 11:52:28 -0700

    moving

--- python/helper.py
+++ python/helper.py
@@ -22,5 +22,14 @@ def add_coordinates(coord1, coord2):
 	coord3 = (coord1[0] + coord2[0], coord1[1] + coord2[1])
 	return coord3
 
+def get_ellipse_points(origin, sizex, sizey) -> set:
+	center_x = origin[0]
+	center_y = origin[1]
+
+	x = 0
+	y = sizey
+
+	
+
 if __name__ == "__main__":
 	print (linearConversion(110, 0, 100, 0, 10))
